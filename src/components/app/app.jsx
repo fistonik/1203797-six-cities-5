a1779bb3478a40214d2526b2bcb9f3a9f9d0a080
Антон <fistonik@yandex.ru>
2020-10-03 12:04:27 +0400

    module2-task1 - добавил компоненты оставшихся страниц,настроил роутинг основных страниц

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -1,14 +1,33 @@
 import React from "react";
 import PropTypes from "prop-types";
+import {BrowserRouter, Switch, Route} from "react-router-dom";
 import Main from "../main/main";
+import AuthScreen from "../auth-screen/auth-screen";
+import FavoritesScreen from "../favorites-screen/favorites-screen";
+import OfferScreen from "../offer-screen/offer-screen";
 
 const App = (props) => {
   const {countOffers} = props;
 
   return (
-    <Main
-      countOffers={countOffers}
-    />
+    <BrowserRouter>
+      <Switch>
+        <Route exact path="/">
+          <Main
+            countOffers={countOffers}
+          />
+        </Route>
+        <Route exact path="/login">
+          <AuthScreen/>
+        </Route>
+        <Route exact path="/favorites">
+          <FavoritesScreen/>
+        </Route>
+        <Route exact path="/offer/:id?">
+          <OfferScreen/>
+        </Route>
+      </Switch>
+    </BrowserRouter>
   );
 };
 
